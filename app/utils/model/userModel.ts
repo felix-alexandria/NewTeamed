672b9212c9e63acac86066389a05ead5e6db7342
felix-alexandria <jesmiah0603@gmail.com>
2024-06-11 16:28:36 +0100

    Backend

---
+++ app/utils/model/userModel.ts
@@ -0,0 +1,37 @@
+import { model, Schema, Document, models } from "mongoose";
+
+interface iUsers {
+  name: string;
+  email: string;
+  password: string;
+  homeAddress: string;
+  telephoneNo: number;
+}
+
+interface iUsersData extends iUsers, Document {}
+
+const userModel = new Schema<iUsersData>(
+  {
+    name: {
+      type: String,
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    password: {
+      type: String,
+    },
+    homeAddress: {
+      type: String,
+    },
+    telephoneNo: {
+      type: Number,
+    },
+  },
+  { timestamps: true }
+);
+
+const users = models.users || model<iUsersData>("users", userModel);
+
+export default users;
